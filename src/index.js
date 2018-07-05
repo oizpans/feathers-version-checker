629131791abfeb039908463bffa6fb95aed3f1ae
nateblog <neilanthonyteblog@gmail.com>
2018-07-05 17:18:53 +0800

    added transpiler

---
+++ src/index.js
@@ -0,0 +1,39 @@
+require("babel-polyfill");
+const { checkVersion } = require('version_checker');
+const { paramsForServer } = require('feathers-hooks-common');
+const errors = require('feathers-errors');
+
+module.exports.checkForLatestVersion = function(appVersion) {
+  return async function(context){
+    if(typeof context === 'undefined') {
+      throw new Error('Context is not defined!');
+    } else if(typeof appVersion === 'undefined' || appVersion === '') {
+      throw new Error("Function 'checkForLatestVersion' must contain a parameter e.g checkForLatestVersion('1.2.0')");
+    } else {
+      let { params } = context;
+      const { query } = paramsForServer({currentAppVersion: appVersion});
+      params.query = {...params.query, ...query};
+      return context;
+    }
+  }
+};
+
+module.exports.latestVersionResponse = function(minimumVersion) {
+  return async function(context){
+    if(typeof context === 'undefined') {
+      throw new Error('Context is not defined!');
+    } else if(typeof minimumVersion === 'undefined' || minimumVersion === '') {
+      throw new Error("Function 'latestVersionResponse' must contain a parameter e.g latestVersionResponse('1.2.0')");
+    } else {
+      if(context.params.currentAppVersion) {
+        if(checkVersion({currentAppVersion: context.params.currentAppVersion, minimumRequiredAppVersion: minimumVersion}).updated) {
+          return context;
+        } else {
+          throw new errors.BadRequest('OUTDATED', {
+            outdated: true
+          });
+        }
+      }
+    }
+  }
+};
