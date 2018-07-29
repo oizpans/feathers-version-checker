1097433c028c293f17b187c56aab7646667583ce
rod <rod@snapzio.com>
2018-07-29 23:03:06 +0800

    setting up tests

---
+++ test/index.js
@@ -0,0 +1,5 @@
+const VersionChecker = require('./version_checker');
+
+describe('this i a test', () => {
+  VersionChecker();
+});
---
+++ test/version_checker/index.js
@@ -0,0 +1,7 @@
+module.exports = function VersionChecker() {
+  describe('this i a test', () => {
+    test('it should work', () => {
+      expect(true).toEqual(true);
+    });
+  });
+};
