5847c2865b36fa91cc7e3939d6437b9cd2b3d749
rod <rod@snapzio.com>
2018-07-30 00:03:01 +0800

    implemented testing

--- test/version_checker/index.js
+++ test/version_checker/index.js
@@ -1,9 +1,38 @@
-const empty = require('./empty');
-const validations = require('./validations');
+const { checkVersion } = require('../../lib/version_checker');
 
 module.exports = function VersionChecker() {
   describe('Version Checker', () => {
-    empty();
-    validations();
+    test('When dataVersion is empty', () => {
+      const dataVersion = {};
+
+      const result = checkVersion(dataVersion);
+
+      expect(result.updated).toEqual(true);
+      expect(result.incompatible).toEqual([]);
+    });
+
+    test('With MINOR issues', () => {
+      const dataVersion = {
+        currentAppVersion: '2.0.0',
+        minimumRequiredAppVersion: '2.1.0',
+      };
+
+      const result = checkVersion(dataVersion);
+
+      expect(result.updated).toEqual(false);
+      expect(result.incompatible).toContainEqual({ with: 'minimumAllowedAppVersion', changes: 'MINOR' });
+    });
+
+    test('With MAJOR issues', () => {
+      const dataVersion = {
+        currentAppVersion: '1.0.0',
+        minimumRequiredAppVersion: '2.0.0',
+      };
+
+      const result = checkVersion(dataVersion);
+
+      expect(result.updated).toEqual(false);
+      expect(result.incompatible).toContainEqual({ with: 'minimumAllowedAppVersion', changes: 'MAJOR' });
+    });
   });
 };
